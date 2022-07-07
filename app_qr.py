5e571c00590df3804f364e310efd34448d81011e
blomjoe <64746667+blomjoe@users.noreply.github.com>
2022-07-08 00:45:31 +0300

    fine tuning

--- app_qr.py
+++ app_qr.py
@@ -3,9 +3,9 @@
 import qrcode
 from PIL import Image
 
-value = input("\nPlease enter content for QR code:\n\n")
+value = input("\nenter content for QR code:\n\n")
 
 img = qrcode.make(value)
-img.save("QR.jpg")
+img.save("QR_export.jpg")
 
 print("\nQR code generated in program path.\n")
